65fbdef0d327b9fe7798e63adfc8b7fe6b289568
Danilo Ribeiro <danriba@gmail.com>
2023-11-27 17:23:36 -0300

    Desenvolvimento 2

---
+++ 03_preparacao_html_css/02_desenvolvimento.py
@@ -0,0 +1,13 @@
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+# Imprimir a quantidade de elementos
+print("Quantidade de elementos:", len(lista_musicos))
+
+# Imprimir o dado no índice 2
+print("Dado no índice 2:", lista_musicos[2])
+
+# Imprimir o dado no índice 9
+print("Dado no índice 9:", lista_musicos[9])
+
+# Imprimir o dado no índice 14
+print("Dado no índice 14:", lista_musicos[14])
