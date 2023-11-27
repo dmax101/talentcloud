b33af90cfebdacbebd3ff8297819ba39ac41f311
Danilo Ribeiro <danriba@gmail.com>
2023-11-27 15:19:56 -0300

    Desenvolvimento 01

---
+++ 03_preparacao_html_css/01_desenvolvimento.py
@@ -0,0 +1,20 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal
+# usando o comando print(). O primeiro array deve conter os produtos de uma loja da sua escolha
+# (loja de comida, materiais de construção, música, etc). O segundo array deve conter os anos
+# de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear
+# cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos,
+# números inteiros, floats).Trabalhe esse código no Google Colab, e compartilhe o link do 
+# projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
+
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal
+# usando o comando print().
+
+# Declare e inicialize o primeiro array, que representa os produtos de uma loja.
+produtos_loja = ['Macarronada 500g', 'Biscoito Amoroso', 'Fita Isolante 3m', 'Lixo Branco', 'Madeira']
+
+# Declare e inicialize o segundo array, que representa os anos de nascimento de familiares e amigos.
+anos_nascimento = [1980, 1990, 2000, 2010, 2020]
+
+# Imprima os arrays no terminal usando o comando print().
+print("Produtos da loja:", produtos_loja)
+print("Anos de nascimento:", anos_nascimento)
