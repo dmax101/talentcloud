c288dd17c79830245f681471b8a027a0255daf90
Danilo Ribeiro <danriba@gmail.com>
2023-11-27 17:27:58 -0300

    Desenvolvimento 3

---
+++ 03_preparacao_html_css/03_desenvolvimento.py
@@ -0,0 +1,7 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+# Iterar sobre a lista de produtos
+for produto in lista_produtos:
+    # Imprimir a frase com o produto
+    print("Temos", produto, "à venda!")
+    
