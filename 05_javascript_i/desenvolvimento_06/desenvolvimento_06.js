d28dd7d7054ea0076864f526d260608de60a23ee
Danilo Ribeiro <danriba@gmail.com>
2024-02-05 19:19:31 -0300

    desenvolvimento_06

---
+++ 05_javascript_i/desenvolvimento_06/desenvolvimento_06.js
@@ -0,0 +1,9 @@
+function oneCompilerFunction(a, b) {
+    console.log(`${a} + ${b} = ${a + b}
+${a} - ${b} = ${a - b}
+${a} x ${b} = ${a * b}
+${a} / ${b} = ${a / b}`
+    );
+}
+
+oneCompilerFunction(4,5)
